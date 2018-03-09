f90423b65d58e87e622d853e1a46d533e586dec1
Skye <36886578+Zzunky@users.noreply.github.com>
2018-03-09 15:41:39 +0800

    Create jvs.js

---
+++ js/jvs.js
@@ -0,0 +1,43 @@
+function firstContent()
+	{
+		x=document.getElementById("demo");
+		x.innerHTML="Able was I ere I saw Elba.";
+	}
+		function changeContent()
+	{
+		x=document.getElementById("demo");
+		x.innerHTML="Elba saw I ere I was Able";
+	}
+		function changeStyle()
+	{
+	x=document.getElementById("intro1");
+	y=document.getElementById("intro2");
+	x.style.color="coral";
+	x.style.fontSize="20px";
+	y.style.color="khaki";
+	y.style.fontSize="14px";
+	}
+	function changeImage()
+	{
+	element=document.getElementById("myimage")
+	if(element.src.match("bllubon"))
+	{
+		element.src="images/blluboff.gif";
+	}
+	else
+	{
+		element.src="images/bllubon.gif";
+		}
+	}	
+	function checkFun()
+{
+	var x=document.getElementById("txtbox1").value;
+	if(x==""||isNaN(x))         //isNaN() isdigit
+	{
+		alert("That's not a number");
+	}
+	else
+        {
+                alert("The input result is"+x);
+        }
+}
